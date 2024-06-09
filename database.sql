d70858d42004e37838ab2ee9cc3135fc53173b5e
EEE <157961276+eeevilll@users.noreply.github.com>
2024-06-09 17:03:11 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,14 +1,14 @@
-/*connect to data base root mysql*/
+--connect to data base root mysql
 mysql -u root -p
-/*print all data bases in localhost*/
+--print all data bases in localhost
 SHOW TABLES;
-/*create data base  DB --you can use another name*/
+--create data base  DB --you can use another name
 CREATE DATABASE IF NOT EXISTS DB;
-/*now you will find your database that you created */
+--now you will find your database that you created 
 SHOW TABLES;
-/*use data base BD to create tables*/
+--use data base BD to create tables
 USE DB;
-/*create table */
+--create table 
 CREATE TABLE IF NOT EXISTS user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
